3a419629beaf8e0e6b627ff5a69ec8b88ac585b4
Waruni Gunasena <121920439+WaruniGunasena@users.noreply.github.com>
2025-09-24 12:24:25 +0530

    Backend configuration setup

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,21 @@
+const { Socket } = require("dgram");
 const express = require("express");
 const app = express();
+
+const server = require("http").createServer(app);
+const { Server } = require("socket.io");
+
+const io = new Server(server);
+
+//routes
+app.get("/",(req,res) => {
+  res.send("This is mern realtime whiteboard sharing app official server by Waruni Gunasena");
+})
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+});
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => console.log("server is running on http://localhost:5000"));
